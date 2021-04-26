74612475e1879be8bf9214d2652bb1dbd1c78d13
Nitesh jha <nitesh.jha@fluper.in>
2021-04-26 16:39:03 +0530

    Add Duration

--- CircularProgressBar/ViewController.swift
+++ CircularProgressBar/ViewController.swift
@@ -17,7 +17,10 @@ class ViewController: UIViewController {
         circularProgress.trackClr = UIColor.black
         circularProgress.progressClr = UIColor.red
         let progress = (1.00 * (60 / 100))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
             self.circularProgress.setProgressWithAnimation(duration: 1.0, value: Float(progress))
+        }
     }
 }
 
